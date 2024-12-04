7397e29e3f62944d18e6ebcc674032cf1c9c4572
Shigekatsu Sasaki <shigekatsu.sasaki@relivewear.jp>
2024-12-04 09:40:29 +0900

    発注伝票の外部化

--- common.js
+++ common.js
@@ -1,3 +1,133 @@
-// 検証用のダミーコード
-console.log("23:29 common.js is called");
+// ==UserScript==
+// @name         One's Closet用共通処理
+// @namespace    http://tampermonkey.net/
+// @version      2024-12-04
+// @description  try to take over the world!
+// @author       Shigekatsu Sasaki
+// @match        https://ones-closet.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ones-closet.com
+// @grant        none
+// ==/UserScript==
 
+let jp = {
+    relivewear: {
+        tm: {
+            /**
+             * メッセージを表示する。
+             * @param メッセージ
+             * @param panelWidth パネル幅(px)
+             */
+            showMessage: function(message, panelWidth = 400){
+                let $el = $("#tm_message");
+
+                if ($el.length) {
+                    $el.text(message);
+                    $el.show();
+                }
+                else {
+                    const left = $(window).width() / 2 - panelWidth / 2 + "px";
+
+                    $el = $("<div id='tm_message' class='alert alert-info' role='alert'></div>");
+                    $el.css("position", "absolute")
+                    .css("width", panelWidth + "px")
+                    .css("top", "5px")
+                    .css("left", left)
+                    .css("z-index", "10000");
+                    $el.text(message);
+
+                    $("body").append($el);
+                }
+            },
+
+            /**
+             * メッセージを非表示にする。
+             */
+            hideMessage: function(){
+                $("#tm_message").hide();
+            },
+
+            /**
+             * 確認パネルを表示する。
+             * @param メッセージ
+             * @param callbackMap ボタン名とボタンクリック時に実行するコールバック関数の連想配列
+             * @param panelWidth パネル幅(px)
+             */
+            confirm: function(message, callbackMap, panelWidth = 400){
+                /*
+<div id='tm_confirmdialog' class='panel panel-default' style='position:absolute;width:400px;top:5px;left:400px;font-size:90%;z-index:10000;'>
+  <div class='panel-heading'>Chrome拡張によるカスタム処理実行</div>
+  <div class='panel-body'>
+    <span id='tm_confirmdialog_message'>自動集計を実行しますか？</span>
+    <div style='text-align:right;'>
+      <button type='button' id='tm_button_execute1' class='btn btn-sm btn-primary' style='margin-right:0.5em;'>はい（すべて）</button>
+      <button type='button' id='tm_button_execute2' class='btn btn-sm btn-primary' style='margin-right:0.5em;'>はい（法人のみ）</button>
+      <button type='button' id='tm_button_cancel' class='btn btn-sm btn-default'>キャンセル</button>
+    </div>
+  </div>
+</div>
+                */
+                let $panel = $("#tm_confirmdialog");
+
+                if ($panel.length) {
+                    $("#tm_confirmdialog_message").text(message);
+
+                    $panel.fadeTo(1000, 1); // 1000msで透明度100%まで
+                }
+                else {
+                    const left = $(window).width() / 2 - panelWidth / 2 + "px";
+
+                    $panel = $("<div id='tm_confirmdialog' class='panel panel-default'></div>");
+                    $panel.css("position", "absolute")
+                    .css("width", panelWidth + "px")
+                    .css("top", "5px")
+                    .css("left", left)
+                    .css("font-size", "90%")
+                    .css("z-index", "10000")
+                    .css("display", "none");
+
+                    const $panel_heading = $("<div class='panel-heading'>Chrome拡張によるカスタム処理実行</div>");
+                    const $panel_body = $("<div class='panel-body'></div>");
+
+                    const $panel_message = $("<span id='tm_confirmdialog_message'></span>");
+                    const $div = $("<div></div>")
+                    .css("text-align", "right")
+                    .css("margin-top", "1em");
+
+                    // callbackMapをループしてボタン名とコールバック関数を設定
+                    let i = 1;
+                    for (let key in callbackMap) {
+                        const id = `tm_button_execute${i}`;
+                        const $button = $(`<button type='button' id='${id}' class='btn btn-sm btn-primary'>${key}</button>`)
+                            .css("margin-right", "0.5em");
+
+                        $button.on("click", function(){
+                            $("#tm_confirmdialog").hide();
+                            callbackMap[key].call();
+                        });
+
+                        $div.append($button);
+                    }
+
+                    const $button_cancel = $("<button type='button' id='tm_button_cancel' class='btn btn-sm btn-default'>キャンセル</button>");
+                    $button_cancel.on("click", function(){
+                        $("#tm_confirmdialog").hide();
+                    });
+
+                    $div.append($button_cancel);
+
+                    $panel_body.append($panel_message);
+                    $panel_body.append($div);
+
+                    $panel.append($panel_heading);
+                    $panel.append($panel_body);
+
+                    $("body").append($panel);
+
+                    $panel.fadeTo(500, 1); // 500msで透明度100%まで
+
+                    $("#tm_confirmdialog_message").text(message);
+                }
+            },
+        }
+    }
+};
--- purchaseOrder.js
+++ purchaseOrder.js
@@ -1,3 +1,107 @@
-// 検証用のダミーコード
-console.log("23:29 purchaseOrder.js is called");
+// ==UserScript==
+// @name         One's Closet発注伝票
+// @namespace    http://tampermonkey.net/
+// @version      2024-12-04
+// @description  try to take over the world!
+// @author       Shigekatsu Sasaki
+// @match        https://ones-closet.com/app/purchaseOrder.php
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ones-closet.com
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    // $.fn.jQuery => 1.11.0
+    // 日付の処理にはmoment.jsを利用
+    // @see https://qiita.com/taizo/items/3a5505308ca2e303c099
+
+    const f1 = function() {
+        // 表示ボタンクリック
+        setTimeout(function(){
+            $("#list_disp").click();
+
+            // 残のある伝票クリック
+            setTimeout(function(){
+                $("#list_remain").click();
+            }, 500);
+        }, 500);
+
+        let count = 0;
+        // 明細が表示されるまで待機
+        const timerId = setInterval(function(){
+            if ($("#list_result tbody tr").length) {
+                $("#list_exp").click(); // 書き出しアイコンのクリック
+                $("a.list_exportFormat")
+                    .filter(function(i, el){
+                        return $(el).text() == "需要予測・発注・在庫管理シート用"
+                    })
+                    .click(); // 「需要予測・発注・在庫管理シート用」の選択
+
+                // CSVのダウンロードのダイアログが出てくるので手動で保存
+
+                clearInterval(timerId);
+
+                setTimeout(function(){
+                    alert("書き出し処理 完了");
+                }, 500);
+            }
+
+            // 500*100ミリ秒（50秒）待っても明細が表示されない場合は処理中断
+            if (100 < count++) {
+                alert("画面遷移に時間がかかりすぎたため処理を中断します。");
+                clearInterval(timerId);
+            }
+        }, 500);
+    };
+
+    const f2 = function() {
+        // 表示ボタンクリック
+        setTimeout(function(){
+            $("#list_disp").click();
+
+            // 残のある伝票クリック
+            setTimeout(function(){
+                $("#list_remain").click();
+            }, 500);
+        }, 500);
+
+        let count = 0;
+        // 明細が表示されるまで待機
+        const timerId = setInterval(function(){
+            if ($("#list_result tbody tr").length) {
+                $("#list_exp").click(); // 書き出しアイコンのクリック
+                $("a.list_exportFormat")
+                    .filter(function(i, el){
+                        return $(el).text() == "納品予定表用"
+                    })
+                    .click(); // 「納品予定表用」の選択
+
+                // CSVのダウンロードのダイアログが出てくるので手動で保存
+
+                clearInterval(timerId);
+
+                setTimeout(function(){
+                    alert("書き出し処理 完了");
+                }, 500);
+            }
+
+            // 500*100ミリ秒（50秒）待っても明細が表示されない場合は処理中断
+            if (100 < count++) {
+                alert("画面遷移に時間がかかりすぎたため処理を中断します。");
+                clearInterval(timerId);
+            }
+        }, 500);
+    };
+
+    setTimeout(function(){
+        jp.relivewear.tm.confirm(
+            "自動書き出しを実行しますか？",
+            {
+                "はい（需要予測・発注・在庫管理シート向け）": f1,
+                "はい（納品予定表向け）": f2
+            },
+            600
+        );
+    }, 1000);
+})();
