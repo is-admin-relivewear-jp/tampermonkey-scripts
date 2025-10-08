2f2c38d42a8fb1f32bfbce6fe88c019ffc236dc1
Shigekatsu Sasaki <shigekatsu.sasaki@relivewear.jp>
2025-10-08 11:25:32 +0900

    * 移動伝票（送付予定表）を追加 * 発注伝票（送付予定表）を追加

--- move.js
+++ move.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         One's Closet移動伝票
 // @namespace    http://tampermonkey.net/
-// @version      2025-07-03
+// @version      2025-10-08
 // @description  try to take over the world!
 // @author       Shigekatsu Sasaki
 // @match        https://ones-closet.com/app/move.php
@@ -60,11 +60,54 @@
         }, 500);
     };
 
+    const f2 = function(){
+        // 表示ボタンクリック
+        setTimeout(function(){
+            $("#list_disp").click();
+
+            setTimeout(function(){
+                $("a.list_queryFormat")
+                    .filter(function(i, el){
+                        return $(el).text() == "送付予定表用"
+                    })
+                    .click();
+            }, 500);
+        }, 500);
+
+        let count = 0;
+        // 明細が表示されるまで待機
+        const timerId = setInterval(function(){
+            if ($("#list_result tbody tr").length) {
+                $("#list_exp").click(); // 書き出しアイコンのクリック
+                $("a.list_exportFormat")
+                    .filter(function(i, el){
+                        return $(el).text() == "送付予定表用"
+                    })
+                    .click();
+
+                // CSVのダウンロードのダイアログが出てくるので手動で保存
+
+                clearInterval(timerId);
+
+                setTimeout(function(){
+                    alert("書き出し処理 完了");
+                }, 500);
+            }
+
+            // 500*100ミリ秒（50秒）待っても明細が表示されない場合は処理中断
+            if (100 < count++) {
+                alert("画面遷移に時間がかかりすぎたため処理を中断します。");
+                clearInterval(timerId);
+            }
+        }, 500);
+    };
+
     setTimeout(function(){
         jp.relivewear.tm.confirm(
             "自動書き出しを実行しますか？",
             {
                 "はい（需要予測・発注・在庫管理シート）": f1,
+                "はい（送付予定表）": f2,
             }
         );
     }, 1000);
--- purchaseOrder.js
+++ purchaseOrder.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         One's Closet発注伝票
 // @namespace    http://tampermonkey.net/
-// @version      2025-02-03
+// @version      2025-10-08
 // @description  try to take over the world!
 // @author       Shigekatsu Sasaki
 // @match        https://ones-closet.com/app/purchaseOrder.php
@@ -94,6 +94,7 @@
         }, 500);
     };
 
+    /*
     const f3 = function() {
         setTimeout(function(){
             // 検索ボタンクリック
@@ -159,6 +160,58 @@
             }
         }, 500);
     };
+    */
+
+    const f4 = function() {
+        // 表示ボタンクリック
+        setTimeout(function(){
+            $("#list_disp").click();
+
+            setTimeout(function(){
+                $("a.list_queryFormat")
+                    .filter(function(i, el){
+                        return $(el).text() == "送付予定表用";
+                    })
+                    .click();
+
+                setTimeout(function(){
+                    const now = moment();
+                    const d = moment().date(1).month(now.month() - 12); // 12か月前の1日
+                    $("#queryFormat_A1").val(d.format("YYYY/MM/DD"));
+
+                    $("#queryFormat_ok").click();
+                }, 500);
+            }, 500);
+        }, 500);
+
+        let count = 0;
+        // 明細が表示されるまで待機
+        const timerId = setInterval(function(){
+            if ($("#list_result tbody tr").length) {
+                $("#list_exp").click(); // 書き出しアイコンのクリック
+                $("a.list_exportFormat")
+                    .filter(function(i, el){
+                        // 納品予定表のExportセットを流用
+                        return $(el).text() == "納品予定表用"
+                    })
+                    .click();
+
+                // CSVのダウンロードのダイアログが出てくるので手動で保存
+
+                clearInterval(timerId);
+
+                setTimeout(function(){
+                    alert("書き出し処理 完了");
+                }, 500);
+            }
+
+            // 500*100ミリ秒（50秒）待っても明細が表示されない場合は処理中断
+            if (100 < count++) {
+                alert("画面遷移に時間がかかりすぎたため処理を中断します。");
+                clearInterval(timerId);
+            }
+        }, 500);
+    };
 
     setTimeout(function(){
         jp.relivewear.tm.confirm(
@@ -166,7 +219,8 @@
             {
                 "はい（需要予測・発注・在庫管理シート）": f1,
                 "はい（納品予定表）": f2,
-                "はい（α自主回収）": f3,
+//                "はい（α自主回収）": f3,
+                "はい（送付予定表）": f4,
             },
             600
         );
